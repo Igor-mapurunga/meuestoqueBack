e59a2bab3ffced019a8ae0faca906f11dc444f7a
Igor Mapurunga <126483989+Igor-mapurunga@users.noreply.github.com>
2025-05-28 09:30:03 -0300

    Add files via upload

---
+++ estoque-app/estoque-app/src/main/java/com/meuEstoque/estoque_app/EstoqueAppApplication.java
@@ -0,0 +1,13 @@
+package com.meuEstoque.estoque_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueAppApplication.class, args);
+	}
+
+}
